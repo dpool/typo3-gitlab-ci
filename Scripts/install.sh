116cb8c1bef0a6f30337428c208d83b27d4018b2
Daniel Thomas <daniel.thomas@dpool.com>
2019-03-20 08:54:58 +0100

    -- quick commit --

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
+## make sure that standard folder is available
 [ -d gitlab-ci-scripts ] || mkdir gitlab-ci-scripts
-if [ ! -f public/typo3conf/AdditionalConfiguration.php ]; then
-    cp vendor/dpool/typo3-gitlab-ci/src/AdditionalConfiguration.php public/typo3conf/
-fi
-if [ ! -f .env ]; then
-    cp vendor/dpool/typo3-gitlab-ci/src/.env-example .env
-fi
-cp vendor/dpool/typo3-gitlab-ci/src/.gitlab-ci.yml.dist .gitlab-ci.yml
-cp vendor/dpool/typo3-gitlab-ci/scripts/after-composer.sh gitlab-ci-scripts/_after-composer.sh
-cp vendor/dpool/typo3-gitlab-ci/scripts/build-extensions.sh gitlab-ci-scripts/_build-extensions.sh
-cp vendor/dpool/typo3-gitlab-ci/scripts/pre-deploy.sh gitlab-ci-scripts/_pre-deploy.sh
-cp vendor/dpool/typo3-gitlab-ci/scripts/rsync-build-excludes.txt gitlab-ci-scripts/_rsync-build-excludes.txt
-cp vendor/dpool/typo3-gitlab-ci/scripts/rsync-deploy-excludes.txt gitlab-ci-scripts/_rsync-deploy-excludes.txt
+
+## copy additional configuration for TYPO3
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/AdditionalConfiguration.php public/typo3conf/
+
+## create .env file
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/.env-example .env
+
+## symlink the CI/CD yml configuration
+ln -s vendor/dpool/typo3-gitlab-ci/Configuration/.gitlab-ci.yml .gitlab-ci.yml
+
+## copy empty files for customising of CI/CD processes in an individual project
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/after-composer.sh gitlab-ci-scripts/after-composer.sh
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/build-extensions.sh gitlab-ci-scripts/build-extensions.sh
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/pre-deploy.sh gitlab-ci-scripts/pre-deploy.sh
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/rsync-build-excludes.txt gitlab-ci-scripts/rsync-build-excludes.txt
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/rsync-deploy-excludes.txt gitlab-ci-scripts/rsync-deploy-excludes.txt
+
+## copy npm infrastructure
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/package.json package.json
+cp vendor/dpool/typo3-gitlab-ci/Scaffolding/Gulpfile.js Gulpfile.js
