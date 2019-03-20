0c04f224ae7249250e7e2db5f96bf96e5b9de551
Daniel Thomas <daniel.thomas@dpool.com>
2019-03-20 09:27:46 +0100

    -- quick commit --

--- Scripts/npm-dependencies.sh
+++ Scripts/npm-dependencies.sh
@@ -1,9 +1,8 @@
-npm install gulp@^3.9.1
-npm install gulp-clean-css@^3.9.1
-npm install gulp-concat@^3.9.1
-npm install gulp-sass@^3.9.1
-npm install gulp-uglify@^3.9.1
-npm install gulp@^3.9.1
-npm install bootstrap@^4.3.1
-npm install jquery@^3.3.1
-npm install popper.js@^1.14.7
+npm install gulp@4.0.0
+npm install gulp-clean-css@4.0.
+npm install gulp-concat@2.6.1
+npm install gulp-sass@4.0.2
+npm install gulp-uglify@3.0.2
+npm install bootstrap@4.3.1
+npm install jquery@3.3.1
+npm install popper.js@1.14.7
