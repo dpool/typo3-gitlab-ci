7c950f9a3144f85b2c8da43a0f3de21443208b54
Daniel Thomas <daniel.thomas@dpool.com>
2019-03-20 08:18:18 +0100

    -- quick commit --

--- src/AdditionalConfiguration.php
+++ src/AdditionalConfiguration.php
@@ -1,3 +1,7 @@
 <?php
-
+/*
+ * This call will read the .env file located in the Root-Directory of
+ * our composer installation and use it to populate crucial TYPO3-variables
+ * in the LocalConfiguration of the instance
+ */
 (new \Dpool\Typo3GitlabCi\ConfigLoader())->populate();
